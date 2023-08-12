2337f2c4be158004a393facc5f31d2f7e18a593f
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2023-08-12 16:16:42 +0300

    -9i-09898weqr

--- js/task-08.js
+++ js/task-08.js
@@ -4,17 +4,18 @@ form.addEventListener("submit", onSubmit)
 
 function onSubmit(evt){
     evt.preventDefault()
-    const obj = {}
     const emailInput = evt.currentTarget.elements.email.value
     const passInput = evt.currentTarget.elements.password.value
     const email =  evt.currentTarget.elements.email.getAttribute("name")
-console.log(email)
+    const password =  evt.currentTarget.elements.password.getAttribute("name")
     if (emailInput.length === 0 || passInput.length === 0){
         alert("все поля должны быть заполнены")
     }
-console.log(evt.currentTarget.elements.email)
-   obj[email] = emailInput
-   return obj
+const obj = {[email] : emailInput, [password]: passInput}
+console.log(obj)
+evt.currentTarget.reset()
+
+ 
 
 }
 
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+ const input = document.getElementById("controls").firstElementChild
+ const btnCreate = document.querySelector("[data-create]")
+ const btnDestroy = document.querySelector("[data-destroy]")
+
+ const boxes = document.getElementById("boxes")
+
+ btnCreate.addEventListener("click", createBoxes) 
+ btnDestroy.addEventListener("click", destroyBoxes)
+ 
+ let counter = 0
+ const height = 30
+ const width = 30
+
+ function createBox(){
+  const box = document.createElement('div')
+  box.style.width = width + counter + 'px'
+  box.style.height = height + counter + 'px'
+  box.style.backgroundColor = getRandomHexColor()
+  counter += 10
+  return box 
+ }
+
+function createBoxes(amount){
+  amount = input.value;
+
+  const fragment = document.createDocumentFragment()
+  for (let i=1; i<=amount; i+=1){
+    fragment.append(createBox())
+  }
+  return boxes.append(fragment)
+}
+
+function destroyBoxes(){
+  boxes.innerHTML = ""
+  counter = 0
+  input.value = ""
+}
