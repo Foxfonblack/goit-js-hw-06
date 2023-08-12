a9416123090ef07f718047d29503ca3e8b82af71
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2023-08-12 16:15:45 +0300

    piuhouyh;kl

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const backgroundText = document.querySelector(".color")
+const button = document.querySelector(".change-color")
+button.addEventListener("click", onClick)
+
+function onClick(evt){
+  const color = getRandomHexColor()
+  document.body.style.backgroundColor = color
+  backgroundText.textContent = color
+}
+
+
